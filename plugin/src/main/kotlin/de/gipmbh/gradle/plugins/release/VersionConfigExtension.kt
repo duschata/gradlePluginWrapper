dd8374949b99eb45bf8db6a77cbf0c2aa0714e76
Thomas Grabietz <grabietz@gipmbh.de>
2023-05-11 07:15:07 +0200

    annotations removed(not necessary here) some convenstions set

--- plugin/src/main/kotlin/de/gipmbh/gradle/plugins/release/VersionConfigExtension.kt
+++ plugin/src/main/kotlin/de/gipmbh/gradle/plugins/release/VersionConfigExtension.kt
@@ -1,37 +1,21 @@
 package de.gipmbh.gradle.plugins.release
 
+import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Optional
-import java.io.File
 
 abstract class VersionConfigExtension {
 
-    @get:Optional
-    abstract var version: String
-
-    @get:Input
-    @get:Optional
+    abstract val version: Property<String>
     abstract val leastVersion: Property<String>
-
-    @get:Input
-    @get:Optional
     abstract val tagPrefix: Property<String>
-
-    @get:Input
-    @get:Optional
     abstract val ignoreUncommittedChanges: Property<Boolean>
-
-    @get:Input
-    @get:Optional
     abstract val useHighestVersion: Property<Boolean>
-
-    @get:Input
-    @get:Optional
     abstract val incrementer: Property<String>
+    abstract val directory: DirectoryProperty
 
-    @get:Input
-    @get:Optional
-    abstract val gitDirectory: Property<File>
-
+    init {
+        tagPrefix.convention("V")
+        ignoreUncommittedChanges.convention(false)
+        useHighestVersion.convention(false)
+    }
 }
